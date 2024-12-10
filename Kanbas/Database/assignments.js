c6f95c1e34867ef0b4963bc20923f5aab45a86f5
Your Name <you@example.com>
2024-12-10 16:18:25 -0500

    mongodb code

--- Kanbas/Database/assignments.js
+++ Kanbas/Database/assignments.js
@@ -1,6 +1,5 @@
 export default [
   {
-    _id: "A101",
     title: "Propulsion Assignment",
     course: "RS101",
     unlock: "2024-05-06T00:00",
@@ -9,7 +8,6 @@ export default [
     points: 75,
   },
   {
-    _id: "A102",
     title: "Combustion Analysis",
     course: "RS101",
     unlock: "2024-05-13T14:00",
@@ -18,7 +16,6 @@ export default [
     points: 50,
   },
   {
-    _id: "A103",
     title: "Nozzle Design Project",
     course: "RS101",
     unlock: "2024-05-20T00:00",
@@ -27,7 +24,6 @@ export default [
     points: 100,
   },
   {
-    _id: "A201",
     title: "Aerodynamics Quiz",
     course: "RS102",
     unlock: "2024-11-15T14:00",
@@ -36,7 +32,6 @@ export default [
     points: 80,
   },
   {
-    _id: "A202",
     title: "Flow Analysis",
     course: "RS102",
     unlock: "2024-11-15T00:00",
@@ -45,7 +40,6 @@ export default [
     points: 100,
   },
   {
-    _id: "A203",
     title: "Heating Analysis",
     course: "RS102",
     unlock: "2024-11-20T00:00",
@@ -54,7 +48,6 @@ export default [
     points: 100,
   },
   {
-    _id: "A301",
     title: "Structural Design Task",
     course: "RS103",
     unlock: "2024-05-06T00:00",
@@ -63,7 +56,6 @@ export default [
     points: 100,
   },
   {
-    _id: "A302",
     title: "Orbital Calculations",
     course: "RS103",
     unlock: "2024-05-13T00:00",
@@ -72,7 +64,6 @@ export default [
     points: 60,
   },
   {
-    _id: "A303",
     title: "Systems Engineering Exam",
     course: "RS103",
     unlock: "2024-05-20T00:00",
--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -3,5 +3,4 @@ import modules from "./modules.js";
 import users from "./users.js";
 import enrollments from "./enrollments.js";
 import assignments from "./assignments.js";
-import quizzes from "./quizzes.js";
-export default { courses, modules, users, enrollments, assignments, quizzes };
+export default { courses, modules, users, enrollments, assignments };
