a72de3bdd044128f74509ee71953f69e363eccbf
pratham-shah28 <psshah2803@gmail.com>
2024-12-11 12:59:15 -0500

    fix

--- Kanbas/Quizzes/dao.js
+++ Kanbas/Quizzes/dao.js
@@ -4,5 +4,21 @@ export function findQuizzesByCourseId(courseId) {
     return Database.quizzes.filter((q) => q.course === courseId);
 }
 
+export function createQuiz(quiz) {
+    const newQuiz = { ...quiz, _id: Date.now().toString() };
+    Database.quizzes = [...Database.quizzes, newQuiz];
+    return newQuiz;
+}
 
 
+export function updateQuiz(quizId, quizUpdates) {
+    const { quizzes } = Database;
+    const quiz = quizzes.find((quiz) => quiz._id === quizId);
+    Object.assign(quiz, quizUpdates);
+    return quiz;
+}
+
+export function deleteQuiz(quizId) {
+    const { quizzes } = Database;
+    Database.quizzes = quizzes.filter(q => q._id !== quizId);
+}
