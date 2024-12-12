a7bdeb35e6397c535c544259474f79590fd8b5aa
Your Name <you@example.com>
2024-12-11 19:16:00 -0500

    fixed index.js

--- Kanbas/Quizzes/dao.js
+++ Kanbas/Quizzes/dao.js
@@ -3,6 +3,3 @@ import Database from "../Database/index.js";
 export function findQuizzesByCourseId(courseId) {
     return Database.quizzes.filter((q) => q.course === courseId);
 }
-
-
-
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import "dotenv/config";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -8,8 +9,10 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
-import QuizRoutes from "./Kanbas/Quizzes/routes.js";
+import mongoose from "mongoose";
 import "dotenv/config";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
@@ -37,8 +40,6 @@ UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 EnrollmentRoutes(app);
-AssignmentRoutes(app);
-QuizRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
